cd6f48b8d8092ee75e3389e21919fc67cd1ff67d
tugrulvural89 <tugrulv89@gmail.com>
2024-05-29 18:44:13 +0300

    v1.0.0

--- productDescAi/settings.py
+++ productDescAi/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['shoupapp.com']
+ALLOWED_HOSTS = ['www.shoupapp.com']
 
 # Application definition
 
